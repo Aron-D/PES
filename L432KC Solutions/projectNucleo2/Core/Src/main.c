8e8338ecbdc98b8a308e4cab7c6d188ca26c4788
ANKouwenhoven <arnoudkouwenhoven345@gmail.com>
2024-05-08 15:49:24 +0200

    Formatting

--- L432KC Solutions/projectNucleo2/Core/Src/main.c
+++ L432KC Solutions/projectNucleo2/Core/Src/main.c
@@ -689,58 +689,43 @@ void Set_LED (int LEDnum, int Red, int Green, int Blue)
 	LED_Data[LEDnum][3] = Blue;
 }
 
-void Set_Brightness (int brightness)  // 0-45
-{
-#if USE_BRIGHTNESS
-
-	if (brightness > 45) brightness = 45;
-	for (int i=0; i<MAX_LED; i++)
-	{
-		LED_Mod[i][0] = LED_Data[i][0];
-		for (int j=1; j<4; j++)
-		{
-			float angle = 90-brightness;  // in degrees
-			angle = angle*PI / 180;  // in rad
-			LED_Mod[i][j] = (LED_Data[i][j])/(tan(angle));
+void Set_Brightness (int brightness) { // 0-45
+	if (USE_BRIGHTNESS) {
+		if (brightness > 45) brightness = 45;
+		for (int i=0; i<MAX_LED; i++) {
+			LED_Mod[i][0] = LED_Data[i][0];
+			for (int j=1; j<4; j++)	{
+				float angle = 90-brightness;  // in degrees
+				angle = angle*PI / 180;  // in rad
+				LED_Mod[i][j] = (LED_Data[i][j])/(tan(angle));
+			}
 		}
 	}
-
-#endif
-
 }
 
-void WS2812_Send ()
-{
+void WS2812_Send () {
 	uint32_t indx=0;
 	uint32_t color;
 
+	for (int i= 0; i<MAX_LED; i++) {
+		if (USE_BRIGHTNESS) {
+			color = ((LED_Mod[i][1]<<16) | (LED_Mod[i][2]<<8) | (LED_Mod[i][3]));
+		} else {
+			color = ((LED_Data[i][1]<<16) | (LED_Data[i][2]<<8) | (LED_Data[i][3]));
+		}
 
-	for (int i= 0; i<MAX_LED; i++)
-	{
-#if USE_BRIGHTNESS
-		color = ((LED_Mod[i][1]<<16) | (LED_Mod[i][2]<<8) | (LED_Mod[i][3]));
-#else
-		color = ((LED_Data[i][1]<<16) | (LED_Data[i][2]<<8) | (LED_Data[i][3]));
-#endif
-
-		for (int i=23; i>=0; i--)
-		{
-			if (color&(1<<i))
-			{
+		for (int i=23; i>=0; i--) {
+			if (color&(1<<i)) {
 				pwmData[indx] = 60;  // 2/3 of 90
+			} else {
+				pwmData[indx] = 30;  // 1/3 of 90
 			}
-
-			else pwmData[indx] = 30;  // 1/3 of 90
 
 			indx++;
 		}
-
 	}
 
-	//HAL_UART_Transmit(&huart2, (uint8_t*) pwmData, sizeof(pwmData), HAL_MAX_DELAY);
-
-	for (int i=0; i<50; i++)
-	{
+	for (int i=0; i<50; i++) {
 		pwmData[indx] = 0;
 		indx++;
 	}
